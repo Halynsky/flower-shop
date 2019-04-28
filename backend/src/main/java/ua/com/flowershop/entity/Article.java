bff6522376f4e605a2da45a1dd35cf723516db40
Taras Halynskyi <off.bk90@gmail.com>
2019-04-28 23:29:01 +0300

    fix article entity, fix modules structure

--- backend/src/main/java/ua/com/flowershop/entity/Article.java
+++ backend/src/main/java/ua/com/flowershop/entity/Article.java
@@ -1,19 +1,13 @@
 package ua.com.flowershop.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
-
-import static ua.com.flowershop.util.Constants.DATE_TIME_PATTERN;
 
 @Getter
 @Service
@@ -27,8 +21,8 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
+    @Column(columnDefinition = "varchar(5000)")
     private String content;
-    //    @JsonFormat(pattern = DATE_TIME_PATTERN)
     private LocalDateTime created = LocalDateTime.now();
     private String logo;
 
