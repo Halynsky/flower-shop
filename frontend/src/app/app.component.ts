6b600b1cec1073ae92d5a9648b4311990dccd66d
Taras Halynskyi <off.bk90@gmail.com>
2019-05-12 15:21:24 +0300

    add sw update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from "@angular/material";
+import { SwUpdate } from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'flowershop-frontend';
+export class AppComponent implements OnInit {
 
-  constructor(public matIconRegistry: MatIconRegistry) {
+  constructor(public matIconRegistry: MatIconRegistry,
+              private swUpdate: SwUpdate) {
     matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+  }
+
+  ngOnInit() {
+    this.checkSwUpdate();
+  }
+
+  checkSwUpdate() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        // console.log("Service is updated, please reload the page to prevent errors");
+        alert('Доступна нова версія нашого сервісу. Ми повинні оновити сторінку щоб відобразити для вас актуальну версію.');
+        window.location.reload();
+      });
+    }
   }
 
 }
