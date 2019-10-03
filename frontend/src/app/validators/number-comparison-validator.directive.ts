2ffdfe14c94aeb4efe707f770ae571cddbe5a1dc
David <david.babiy1995@gmail.com>
2019-10-03 19:55:15 +0300

    changed number comparison validator to observable

--- frontend/src/app/validators/number-comparison-validator.directive.ts
+++ frontend/src/app/validators/number-comparison-validator.directive.ts
@@ -1,8 +1,9 @@
 import { Directive, forwardRef, Input } from '@angular/core';
-import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS } from '@angular/forms';
+import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
+import { Observable, of } from "rxjs";
 
 @Directive({
-  selector: '[numberComparisonValidator][ngModel][minimum], [numberComparisonValidator][ngModel][maximum]',
+  selector: '[numberComparisonValidator][ngModel][state]',
   providers: [
     {
       provide: NG_ASYNC_VALIDATORS,
@@ -14,33 +15,45 @@ import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS } from '@angular/f
 
 export class NumberComparisonValidator implements AsyncValidator {
 
-  @Input('minimum') min;
-  @Input('maximum') max;
+
+  @Input('state') state: boolean;
 
   constructor() {
   }
 
-  validate(control: AbstractControl): Promise<{ [key: string]: any }> {
-    if (this.max == undefined) {
-      if (control.value <= this.min) {
-        return new Promise((resolve) => {
-          resolve({invalidNumber: true});
-        });
+  validate(control: AbstractControl): Observable<ValidationErrors|null> {
+    if (this.state) {
+      return this.validateMin(control);
+
+    } else {
+      return this.validateMax(control);
+    }
+  }
+
+  validateMin(control): Observable<ValidationErrors|null>  {
+    let max = control.root.get('max');
+    if (control.value !== undefined && control.value !== null && max.value !== undefined && max.value !== null) {
+      if (control.value >= max.value) {
+        return  of({['invalidNumber']: true});
       } else {
-        return new Promise((resolve) => {
-          resolve(null);
-        });
+        return of(null);
       }
-    } else if (this.min == undefined) {
-      if (control.value >= this.max) {
-        return new Promise((resolve) => {
-          resolve({invalidNumber: true});
-        });
+    } else {
+      return of(null);
+    }
+
+  }
+
+  validateMax(control): Observable<ValidationErrors|null>  {
+    let min = control.root.get('min');
+    if (control.value !== undefined && control.value !== null && min.value !== undefined && min.value !== null) {
+      if (control.value <= min.value) {
+        return  of({['invalidNumber']: true});
       } else {
-        return new Promise((resolve) => {
-          resolve(null);
-        });
+        return of(null);
       }
+    } else {
+      return of(null);
     }
   }
 
