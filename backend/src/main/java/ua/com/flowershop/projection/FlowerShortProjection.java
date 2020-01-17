fb97d0ed0d74caf4512ff90347b9006cfc5b9f8c
DavidBabiy <david.babiy1995@gmail.com>
2020-01-17 19:39:08 +0200

    optimization

--- backend/src/main/java/ua/com/flowershop/projection/FlowerShortProjection.java
+++ backend/src/main/java/ua/com/flowershop/projection/FlowerShortProjection.java
@@ -5,6 +5,5 @@ import org.springframework.beans.factory.annotation.Value;
 public interface FlowerShortProjection extends FlowerTinyProjection {
     @Value("#{@flowerSizeRepository.findMinPriceByFlowerId(target.id)}")
     Integer getPriceMin();
-    @Value("#{@flowerSizeRepository.hasAnyAvailableFlowerSize(target.id)}")
     Boolean  getHasAvailableFlowerSize();
 }
--- backend/src/main/java/ua/com/flowershop/repository/FlowerSizeRepository.java
+++ backend/src/main/java/ua/com/flowershop/repository/FlowerSizeRepository.java
@@ -41,7 +41,4 @@ public interface FlowerSizeRepository extends JpaRepository<FlowerSize, Long> {
 
     List<FlowerSizeTinyProjection> findProjectedByIdIn(List<Long> ids);
 
-    @Query("SELECT CASE WHEN MAX(fs.amount - fs.reserved) > 0 THEN true ELSE false END FROM FlowerSize fs WHERE fs.flower.id = :flowerId")
-    Boolean hasAnyAvailableFlowerSize(Long flowerId);
-
 }
