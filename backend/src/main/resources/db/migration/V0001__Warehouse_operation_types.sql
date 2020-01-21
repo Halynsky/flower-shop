a33754ff6cfa2105986d2cd5d08a4a9808c71e00
Taras Halynskyi <off.bk90@gmail.com>
2020-01-21 11:50:56 +0200

    add warehouse operation types

---
+++ backend/src/main/resources/db/migration/V0001__Warehouse_operation_types.sql
@@ -0,0 +1,6 @@
+INSERT INTO warehouse_operation_types (direction, operation_type) VALUES ('IN', 'GOODS_ARRIVAL');
+INSERT INTO warehouse_operation_types (direction, operation_type) VALUES ('IN', 'RETURN');
+INSERT INTO warehouse_operation_types (direction, operation_type) VALUES ('OUT', 'SALE');
+INSERT INTO warehouse_operation_types (direction, operation_type) VALUES ('OUT', 'EXTERNAL_SALE');
+INSERT INTO warehouse_operation_types (direction, operation_type) VALUES ('OUT', 'GIFT');
+INSERT INTO warehouse_operation_types (direction, operation_type) VALUES ('OUT', 'DEFECT');
