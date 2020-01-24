e4fc751f09309bc3b37c13772a6f875c698859a0
Taras Halynskyi <off.bk90@gmail.com>
2020-01-24 11:02:12 +0200

    fix group ddl

--- backend/src/main/resources/db/migration/V0004__Greate_groups.sql
+++ backend/src/main/resources/db/migration/V0004__Greate_groups.sql
@@ -5,9 +5,10 @@ create table groups
     id bigserial not null
         constraint groups_pkey
             primary key,
-    name varchar(255) not null,
-    name_original varchar(255) not null,
-    name_single varchar(255) not null,
+    name varchar(255),
+    name_original varchar(255),
+    name_original_single varchar(255),
+    name_single varchar(255),
     flower_type_id bigint
         constraint fkb9vuttw4lkeygajpyf5uxbbfn
             references flower_types
