545970a23e2e7dd29eaaba9399d2e926386f0bb9
Taras Halynskyi <off.bk90@gmail.com>
2020-02-09 09:25:20 +0200

    Fix service workers (#45)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from "@angular/material";
 import { SwUpdate } from "@angular/service-worker";
 import { interval } from "rxjs";
-import { environment } from "../environments/environment";
 
 import * as moment from 'moment'
 import 'moment/locale/uk'
@@ -24,8 +23,17 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.checkSwUpdate();
+  }
 
-    if (environment.production) {
+  checkSwUpdate() {
+
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe(() => {
+        alert('Доступна нова версія нашого сервісу. Ми повинні оновити сторінку щоб відобразити для вас актуальну версію.');
+        window.location.reload();
+      });
+
       interval(this.EACH_MINUTE_INTERVAL).subscribe(() => {
         this.swUpdate.checkForUpdate().then(() => {
           console.log('SW checking for updates');
@@ -33,15 +41,8 @@ export class AppComponent implements OnInit {
       });
 
     }
-  }
 
-  checkSwUpdate() {
-    if (this.swUpdate.isEnabled) {
-      this.swUpdate.available.subscribe(() => {
-        alert('Доступна нова версія нашого сервісу. Ми повинні оновити сторінку щоб відобразити для вас актуальну версію.');
-        window.location.reload();
-      });
-    }
   }
 
 }
+
