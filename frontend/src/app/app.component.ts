cada333ef6d017ea3e80253b2e762063b351fcf2
Taras Halynskyi <off.bk90@gmail.com>
2020-09-14 00:34:53 +0300

    clean up service workers

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SwUpdate } from "@angular/service-worker";
-import { interval } from "rxjs";
 import * as moment from 'moment'
 import 'moment/locale/uk'
 import { MatIconRegistry } from "@angular/material/icon";
@@ -14,33 +12,12 @@ export class AppComponent implements OnInit {
 
   EACH_MINUTE_INTERVAL = 60 * 1000;
 
-  constructor(public matIconRegistry: MatIconRegistry,
-              private swUpdate: SwUpdate) {
+  constructor(public matIconRegistry: MatIconRegistry) {
     matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
     moment.locale('uk');
   }
 
   ngOnInit() {
-    this.checkSwUpdate();
-  }
-
-  checkSwUpdate() {
-
-    if (this.swUpdate.isEnabled) {
-
-      this.swUpdate.available.subscribe(() => {
-        alert('Доступна нова версія нашого сервісу. Ми повинні оновити сторінку щоб відобразити для вас актуальну версію.');
-        window.location.reload();
-      });
-
-      interval(this.EACH_MINUTE_INTERVAL).subscribe(() => {
-        this.swUpdate.checkForUpdate().then(() => {
-          console.log('SW checking for updates');
-        });
-      });
-
-    }
-
   }
 
 }
