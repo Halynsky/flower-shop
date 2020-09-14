5b4563e31c6fe3f88b41015b12c946dfe1eabdbd
Taras Halynskyi <off.bk90@gmail.com>
2020-09-14 08:31:39 +0300

    fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -85,7 +85,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     {
       provide: 'SocialAuthServiceConfig',
       useValue: {
-        autoLogin: false,
+        autoLogin: true,
         providers: [
           {
             id: FacebookLoginProvider.PROVIDER_ID,
