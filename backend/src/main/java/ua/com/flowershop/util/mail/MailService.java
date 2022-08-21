ffcf4360e07aa6e8f68918ed43e82345a3bb37f0
Taras Halynskyi <off.bk90@gmail.com>
2022-08-22 00:43:40 +0300

    upgrade Spring

--- backend/src/main/java/ua/com/flowershop/util/mail/MailService.java
+++ backend/src/main/java/ua/com/flowershop/util/mail/MailService.java
@@ -1,7 +1,7 @@
 package ua.com.flowershop.util.mail;
 
 import lombok.extern.slf4j.Slf4j;
-import nz.net.ultraq.thymeleaf.LayoutDialect;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
